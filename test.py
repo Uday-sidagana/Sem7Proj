e8e4a37fe26ce62f23833b75148ec0ee829788da
Uday-sidagana <uday.sidgana@gmail.com>
2024-11-09 07:09:23 +0530

    Test Successful

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="YOUR_API_KEY")
+genai.configure(api_key="")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Explain how AI works")
 print(response.text)
